d9d044517a4b9a215e17fe98f2cf98fa47c6c3a7
YinYang117 <slcryan2010@yahoo.com>
2022-04-18 17:56:14 -0600

    refactor

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -1,15 +1,31 @@
 def parse(string):
-    if string == "I":
-        return 1
-    elif string == "II":
-        return 2
-    elif string == "III":
-        return 3
-    elif string == "IV":
-        return 4
-    elif string == "V":
-        return 5
-    elif string == "VI":
-        return 6
-    elif string == "VII":
-        return 7
+    num_list = []
+    for char in string:
+        if char == "I":
+            num_list.append(1)
+        elif char == "V":
+            num_list.append(5)
+    i = 0
+    while i < len(num_list):
+        if i < (len(num_list) - 1):
+            if num_list[i] < num_list[i + 1]:
+                num_list[i] *= -1
+        i += 1
+    return sum(num_list)
+    
+
+    # i = 0
+    # while i < len(string):
+    #     if string[i] == "I":
+    #         i_counter += 1
+    #     elif string[i] == "V":
+    #         v_counter += 1
+
+    #         shorthand = True
+    #     i += 1
+        
+    # if shorthand == False:  
+    #     return i_counter + 5 * v_counter
+    # else:
+    #     print("all the stuff", shorthand, i_counter, v_counter)
+    #     return i_counter - 5 * v_counter
