edbdbe203ec7404626ddbaf26880d2196b155933
Philip McCrickard <pcricket10@gmail.com>
2022-04-18 18:57:55 -0400

    test 4

--- test/test_roman_numerals_unittest.py
+++ test/test_roman_numerals_unittest.py
@@ -7,15 +7,16 @@ from app.roman_numerals import parse
 class TestRomanNumerals(unittest.TestCase):
     def test_i(self):
         value = parse("I")
-
         self.assertEqual(value, 1)
 
     def test_ii(self):
         value = parse("II")
-
         self.assertEqual(value, 2)
 
     def test_iii(self):
         value = parse("III")
-
         self.assertEqual(value, 3)
+
+    def test_iv(self):
+        value = parse("IV")
+        self.assertEqual(value, 4)
