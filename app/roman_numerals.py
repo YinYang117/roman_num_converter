dd3e3c55ee2c94a43c1349467c920d5594c7e8b6
YinYang117 <slcryan2010@yahoo.com>
2022-04-18 18:00:37 -0600

    refactor2

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -12,20 +12,3 @@ def parse(string):
                 num_list[i] *= -1
         i += 1
     return sum(num_list)
-    
-
-    # i = 0
-    # while i < len(string):
-    #     if string[i] == "I":
-    #         i_counter += 1
-    #     elif string[i] == "V":
-    #         v_counter += 1
-
-    #         shorthand = True
-    #     i += 1
-        
-    # if shorthand == False:  
-    #     return i_counter + 5 * v_counter
-    # else:
-    #     print("all the stuff", shorthand, i_counter, v_counter)
-    #     return i_counter - 5 * v_counter
